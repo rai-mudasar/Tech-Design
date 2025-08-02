83ef53a75d780f9347802879045f218d2ae89f80
rai-mudasar <raimudassar709@gmail.com>
2025-08-02 20:37:33 +0500

    Add Responsive Styling to TogggleTheme Button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -21,7 +21,7 @@ export default function ThemeToggle() {
     return (
         <button 
         onClick={toggleTheme}
-        className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 cursor-pointer absolute z-100 top-5 right-5"
+        className="px-2 py-1.5 sm:p-2 rounded-full bg-gray-200 dark:bg-gray-700 cursor-pointer absolute z-100 top-2 sm:top-5 right-2 sm:right-5"
         >{theme === "dark" ? "🌞" : "🌙"}</button>
     )
 }
